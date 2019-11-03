56cad0641228efb14a127c98a5fc134ec49b48b2
ed_ca <ing.edwincabezas@gmail.com>
2019-11-03 14:30:28 -0500

    "Se configura modelamiento de BD en index.js"

--- index.js
+++ index.js
@@ -1,6 +1,19 @@
 'use strict'
 
+const setupDataBase = require('.lib/db')
+const setupAgentModel = require('./models/agent')
+const setupMetricModel = require('./models/metric')
+
 module.exports = async function (config) {
+  const sequelize = setupDataBase(config)
+  const AgentModel = setupAgentModel(config)
+  const MetricModel = setupMetricModel(config)
+
+  AgentModel.hasMany(MetricModel)
+  MetricModel.belongsTo(AgentModel)
+
+  await sequelize.authenticate() // va esperar hasta obtener una respuesta.
+
   const Agent = {}
   const Metric = {}
 
